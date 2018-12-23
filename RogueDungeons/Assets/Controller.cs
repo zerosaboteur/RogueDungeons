4f040eb77126dcef57ae517746d5f351cfc0147c
Łukasz Góreczny <zerosaboteur@gmail.com>
2018-12-23 19:30:16 +0100

    Added simple controller for player

---
+++ RogueDungeons/Assets/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float playerSpeed = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer(){
+        if(Input.GetKey(KeyCode.W)){
+            transform.Translate(0,playerSpeed * Time.deltaTime,0);
+        }
+        else if(Input.GetKey(KeyCode.S))
+       {
+           transform.Translate(0,-playerSpeed * Time.deltaTime,0);
+       }
+       else if(Input.GetKey(KeyCode.A))
+       {
+           transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
+       }
+       else if(Input.GetKey(KeyCode.D))
+       {
+           transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
+       }
+    }
+}
