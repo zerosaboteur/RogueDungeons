723b1208adfc5e097404dad9a6d8424b92bc0d84
Łukasz Góreczny <zerosaboteur@gmail.com>
2018-12-23 21:19:51 +0100

    Better Player controlls

--- RogueDungeons/Assets/Controller.cs
+++ RogueDungeons/Assets/Controller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public float playerSpeed = 5; 
+    public float playerSpeed = 2; 
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +17,49 @@ public class Controller : MonoBehaviour
         MovePlayer();
     }
 
+    //simple moving controll
     void MovePlayer(){
         if(Input.GetKey(KeyCode.W)){
-            transform.Translate(0,playerSpeed * Time.deltaTime,0);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
+           if(hit.collider == null){
+               transform.Translate(0,playerSpeed * Time.deltaTime,0);
+           }
+           else if(hit.collider.name == "Coliders" && hit.distance > 0.4f){
+                transform.Translate(0,playerSpeed * Time.deltaTime,0);
+           }
         }
         else if(Input.GetKey(KeyCode.S))
        {
-           transform.Translate(0,-playerSpeed * Time.deltaTime,0);
+           //I create array becouse first thing detected is Player
+           RaycastHit2D[] hit = new RaycastHit2D[2];
+           Physics2D.RaycastNonAlloc(transform.position, Vector2.down,hit);
+           Debug.Log(hit[1].distance);
+           if(hit[1].collider == null){
+               transform.Translate(0,-playerSpeed * Time.deltaTime,0);
+           }
+           else if(hit[1].collider.name == "Coliders" && hit[1].distance > 0.9f){
+                transform.Translate(0,-playerSpeed * Time.deltaTime,0);
+           }
        }
        else if(Input.GetKey(KeyCode.A))
        {
-           transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
+           RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left);
+           if(hit.collider == null){
+               transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
+           }
+           else if(hit.collider.name == "Coliders" && hit.distance > 0.4f){
+                transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
+           }
        }
        else if(Input.GetKey(KeyCode.D))
        {
-           transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
+           RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right);
+           if(hit.collider == null){
+               transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
+           }
+           else if(hit.collider.name == "Coliders" && hit.distance > 0.4f){
+                transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
+           }
        }
     }
 }
