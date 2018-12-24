675ee580fd386fcb98e9a30789e290d995b88fa2
Łukasz Góreczny <zerosaboteur@gmail.com>
2018-12-24 15:09:31 +0100

    Controller update

--- RogueDungeons/Assets/Controller.cs
+++ RogueDungeons/Assets/Controller.cs
@@ -21,44 +21,32 @@ public class Controller : MonoBehaviour
     void MovePlayer(){
         if(Input.GetKey(KeyCode.W)){
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
-           if(hit.collider == null){
+           if(hit.distance > 0.2f || hit == false){
                transform.Translate(0,playerSpeed * Time.deltaTime,0);
            }
-           else if(hit.collider.name == "Coliders" && hit.distance > 0.4f){
-                transform.Translate(0,playerSpeed * Time.deltaTime,0);
-           }
         }
-        else if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S))
        {
            //I create array becouse first thing detected is Player
            RaycastHit2D[] hit = new RaycastHit2D[2];
            Physics2D.RaycastNonAlloc(transform.position, Vector2.down,hit);
-           Debug.Log(hit[1].distance);
-           if(hit[1].collider == null){
+           if(hit[1].distance > 0.9f || hit[1] == false){
                transform.Translate(0,-playerSpeed * Time.deltaTime,0);
            }
-           else if(hit[1].collider.name == "Coliders" && hit[1].distance > 0.9f){
-                transform.Translate(0,-playerSpeed * Time.deltaTime,0);
-           }
        }
-       else if(Input.GetKey(KeyCode.A))
+       if(Input.GetKey(KeyCode.A))
        {
            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left);
-           if(hit.collider == null){
+           if(hit.distance > 0.4f || hit == false){
                transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
            }
-           else if(hit.collider.name == "Coliders" && hit.distance > 0.4f){
-                transform.Translate(-playerSpeed * Time.deltaTime,0 ,0);
-           }
        }
-       else if(Input.GetKey(KeyCode.D))
+       if(Input.GetKey(KeyCode.D))
        {
            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right);
-           if(hit.collider == null){
+           Debug.Log(hit);
+           if(hit.distance > 0.4f || hit == false){
                transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
-           }
-           else if(hit.collider.name == "Coliders" && hit.distance > 0.4f){
-                transform.Translate(playerSpeed * Time.deltaTime,0 ,0);
            }
        }
     }
